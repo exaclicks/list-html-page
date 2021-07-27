d316d4f5a12f6c8554b574a83777999af9f112e8
mrbulut <miksmtr@gmail.com>
2021-07-27 16:14:31 +0200

    update

--- router.php
+++ router.php
@@ -1,5 +1,15 @@
 <?php
+$host = $_SERVER["HTTP_HOST"];
+$uri = $_SERVER["REQUEST_URI"];
+$http_referer = $_SERVER["HTTP_REFERER"];
+$uri = $_SERVER["REQUEST_URI"];
+$uri = $_SERVER["REQUEST_URI"];
 
-echo  "site_adresi: " .$_GET["name"];
-
+echo  "host: " .$host;
+echo  "<br>uri: " .$uri;
+echo  "<br>http_referer: " .$http_referer;
+echo  "<br>" ;
+echo  "<br>" ;
+echo  "<br>" ;
+echo "<pre>$_SERVER</pre>";
 ?>
