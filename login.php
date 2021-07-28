46e02894cdb541d955744ba65b23ef88c2528e75
mrbulut <miksmtr@gmail.com>
2021-07-28 14:58:04 +0200

    update

--- login.php
+++ login.php
@@ -3,10 +3,9 @@
 <body>
 
 
-<form action=“/giris" method="post">
 <div class="row">
-<div class="left”>Ticari</div>
-<div class="left"><input name=“giris" type="text" /></div>
+<div class="left">Ticari</div>
+<div class="left"><input name="giris" type="text" /></div>
 </div>
 <div class="row">
 <div class="left">Password</div>
@@ -17,6 +16,5 @@
 <input id="bypassButton" type="hidden" value="Bypass Layer" /></div>
 <div id="loginFailed" class="row error invisible">Benutzername oder Passwort falsch</div>
 <div id="loginDeactivated" class="row error invisible">Benutzerkonto deaktiviert</div>
-</form>
 </body>
 </html>
